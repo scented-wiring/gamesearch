f5e29c12d5ef4bb929a2d34775a705e54da351f7
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-03-20 15:20:16 +0000

    add search button and data fetch

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import axios from "axios";
+
 import "nes.css/css/nes.min.css";
 import "../styles/Searchbar.css";
 
 const Searchbar = () => {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = () => {
+    axios
+      .get(`https://api.rawg.io/api/games?search=${query}`)
+      .then((response) => console.log(response));
+  };
+
   return (
     <div className="Searchbar">
       <div className="nes-field">
@@ -10,8 +21,16 @@ const Searchbar = () => {
           id="search_field"
           className="nes-input"
           placeholder="Enter query"
+          onChange={(e) => setQuery(e.target.value)}
         />
       </div>
+      <button
+        type="button"
+        className="nes-btn is-primary"
+        onClick={handleSearch}
+      >
+        Search
+      </button>
     </div>
   );
 };
