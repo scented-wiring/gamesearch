249ba63fd53cb1112db2b2046a49e6f669b5bbab
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-04-02 23:49:56 +0100

    render page links at top and bottom of SearchResults

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -23,6 +23,54 @@ const SearchResults: React.FC<SearchResultsProps> = ({
 
   generatePageNumbers(Math.ceil(data.count / resultsPerPage));
 
+  const renderPageLinks = () => {
+    return (
+      pagesArray.length > 1 && (
+        <div className="pages">
+          {pagesArray.map((pageNo: number) => {
+            const standardPageLink = (
+              <div
+                key={pagesArray.indexOf(pageNo)}
+                className={pageNo === page ? "page-active" : "page"}
+                onClick={() => setPage(pageNo)}
+              >
+                {pageNo}
+              </div>
+            );
+            if (pagesArray.length > 10) {
+              return (
+                pageNo === 1 && (
+                  <div key="jump-to" className="jump-to">
+                    <div className="label">Page:</div>
+                    <select
+                      required
+                      defaultValue={page}
+                      className="dark_select nes-pointer"
+                      onChange={(e) => setPage(parseInt(e.target.value))}
+                    >
+                      {pagesArray.map((page) => (
+                        <option
+                          key={pagesArray.indexOf(page)}
+                          value={pagesArray.indexOf(page) + 1}
+                        >
+                          {pagesArray.indexOf(page) + 1}
+                        </option>
+                      ))}
+                    </select>
+
+                    <div className="label">of {pagesArray.length}</div>
+                  </div>
+                )
+              );
+            } else {
+              return standardPageLink;
+            }
+          })}
+        </div>
+      )
+    );
+  };
+
   return (
     <div className="SearchResults">
       <div className="nes-container with-title is-centered">
@@ -36,49 +84,7 @@ const SearchResults: React.FC<SearchResultsProps> = ({
               } of ${data.count}`
             : "Whoops!"}
         </p>
-        {pagesArray.length > 1 && (
-          <div className="pages">
-            {pagesArray.map((pageNo: number) => {
-              const standardPageLink = (
-                <div
-                  key={pagesArray.indexOf(pageNo)}
-                  className={pageNo === page ? "page-active" : "page"}
-                  onClick={() => setPage(pageNo)}
-                >
-                  {pageNo}
-                </div>
-              );
-              if (pagesArray.length > 10) {
-                return (
-                  pageNo === 1 && (
-                    <div key="jump-to" className="jump-to">
-                      <div className="label">Page:</div>
-                      <select
-                        required
-                        defaultValue={page}
-                        className="dark_select nes-pointer"
-                        onChange={(e) => setPage(parseInt(e.target.value))}
-                      >
-                        {pagesArray.map((page) => (
-                          <option
-                            key={pagesArray.indexOf(page)}
-                            value={pagesArray.indexOf(page) + 1}
-                          >
-                            {pagesArray.indexOf(page) + 1}
-                          </option>
-                        ))}
-                      </select>
-
-                      <div className="label">of {pagesArray.length}</div>
-                    </div>
-                  )
-                );
-              } else {
-                return standardPageLink;
-              }
-            })}
-          </div>
-        )}
+        {renderPageLinks()}
         {!error.active ? (
           <div key="SearchResults" className="SearchResults-cards">
             {data.results.map((game: Game) => {
@@ -97,6 +103,7 @@ const SearchResults: React.FC<SearchResultsProps> = ({
             </div>
           </div>
         )}
+        {renderPageLinks()}
       </div>
     </div>
   );
